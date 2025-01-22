6661514598ee80e74b398ae8dacfee9024f65948
Dambulik home linux <dambylik@gmail.com>
2025-01-22 13:54:08 +0100

    flood fill added if map is not rectangular

--- src/parsing/fill_map.c
+++ src/parsing/fill_map.c
@@ -12,12 +12,25 @@
 
 #include "../includes/cub3d.h"
 
+/* void	print_map(t_data *data)
+{
+    int i;
+
+    i = 0;
+    while (i < data->map_height)
+    {
+        printf("%s\n", data->map[i]);
+        i++;
+    }
+} */
+
 // Reads the map data from the linked list and places it into the 2D array (*data)->map)
 
 void	fill_map(t_data **data, t_list **list)
 {
 	t_list	*tmp;
 	int		i;
+	int 	j;
 
 	tmp = *list;
 	i = 0;
@@ -29,6 +42,20 @@ void	fill_map(t_data **data, t_list **list)
 		tmp = tmp->next;
 		i++;
 	}
+	// Replace all spaces with '1' (sort of flood fill)
+    i = 0;
+    while (i < (*data)->map_height)
+    {
+        j = 0;
+        while (j < (*data)->map_width)
+        {
+            if ((*data)->map[i][j] == ' ')
+                (*data)->map[i][j] = '1';
+            j++;
+        }
+        i++;
+    }
+	//print_map(*data);
 }
 
 // copy the content of the current node's line into the corresponding row of the map
