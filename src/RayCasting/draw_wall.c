0cbdce5e173d708a395b97ed96069e0f0cfba4a8
Anna Melnikova <amelniko@student.42.fr>
2025-01-26 19:07:44 +0100

    Additional to previous commit

--- src/RayCasting/draw_wall.c
+++ src/RayCasting/draw_wall.c
@@ -4,9 +4,17 @@ void    draw_line(t_mlx *mlx, int screen_line)
 {
     float   wall_top_pxl;
     float   wall_bot_pxl;
+    float   test_dist;
+    float   test_height;
 
     mlx->ray->dist *= cos(norming_angle(mlx->ray->angle - mlx->player[ANGLE]));
+    //test
+    test_dist = mlx->ray->dist;
     mlx->ray->wall_height = (SCREEN_WIDTH / tan(mlx->player[SIGHT]) * (CELL_SIZE / mlx->ray->dist));
+    test_height = mlx->ray->wall_height;
+    (void) test_dist;
+    (void) test_height;
+    //test
     wall_top_pxl = (SCREEN_HIGHT / 2) - (mlx->ray->wall_height / 2);
     wall_bot_pxl = (SCREEN_HIGHT / 2) + (mlx->ray->wall_height / 2);
     if (wall_bot_pxl > SCREEN_HIGHT)
